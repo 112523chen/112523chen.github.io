fc8b9731cd9e2c027200100f63458d8fc9f1da3d
112523chen <112623chen@gmail.com>
2022-03-28 12:18:09 -0400

    update message

--- resources/js/branding.js
+++ resources/js/branding.js
@@ -1,13 +1,20 @@
 const item = document.getElementById('title');
-var count = 0;
+const item2 = document.getElementById('branding')
 
 item.addEventListener('click',ele =>{
     if(item.style.color == ''){
-        item.children[0].style.color = 'lightblue';
+        item.children[0].style.color = '#bb6069';
         item.children[1].style.color = '#bb6069';
     }
-    if((item.children[0].style.color == 'lightblue')&(count > 0)){
-        item.children[0].innerHTML = 'Kit Kat &hearts;';
+})
+item2.addEventListener('click',ele =>{
+    if(item2.innerHTML == '&lt;/112523chen&gt;'){
+        item2.style.color = 'lightblue';
+        item2.innerHTML = 'Kit Kat &hearts;';
+        item2.style.paddingTop = '10%'
+    } else if (item2.innerHTML == 'Kit Kat ♥'){
+        item2.innerHTML = '&lt;/112523chen&gt;';
+        item2.style.color = '';
+        item2.style.paddingTop = '';
     }
-    count++;
 })
