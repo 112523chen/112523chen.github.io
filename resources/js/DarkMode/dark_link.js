70aa731b85961b9914572b49a0f90eb69632be6c
112523chen <112623chen@gmail.com>
2022-04-04 11:11:26 -0400

    update

--- resources/js/DarkMode/dark_link.js
+++ resources/js/DarkMode/dark_link.js
@@ -1,7 +1,7 @@
 const back = document.body;
 const main_b = document.getElementById("change");
 const link_b = document.getElementsByClassName("button")
-function f(){
+function changeMode(){
     if(main_b.innerHTML == 'Dark Mode'){
         back.style.backgroundColor = '#242933';
         main_b.innerHTML = 'Light Mode';
