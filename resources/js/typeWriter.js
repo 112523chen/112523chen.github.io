f10d171651c5b216fac77e4faffd5af262e0cc7f
Alex Chen <112623chen@gmail.com>
2022-11-03 05:46:34 -0400

    🚀✨ new(): type writer effect

---
+++ resources/js/typeWriter.js
@@ -0,0 +1,36 @@
+const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = ['Data Scientist', 'Web Developer','Data Science Fellow @ CUNY Tech Prep'];
+const ele = document.getElementById('typewriter');
+let sleepTime = 100;
+let curPhrasesIndex = 0;
+
+const writeLoop = async () => {
+    while(true) {
+        let curWord = phrases[curPhrasesIndex];
+
+        for(let i=0; i < curWord.length; i++) {
+            ele.innerText = curWord.substring(0, i+1);
+            await sleep(sleepTime);
+        }
+
+        await sleep(sleepTime * 10);
+
+        for(let i = curWord.length; i > 0; i--){
+            ele.innerText = curWord.substring(0, i-1);
+            await sleep(sleepTime);
+        }
+
+        await sleep(sleepTime * 5);
+
+        if(curPhrasesIndex === phrases.length - 1){
+            curPhrasesIndex = 0;
+        } else{
+            curPhrasesIndex++;
+        }
+    }
+};
+
+writeLoop();
