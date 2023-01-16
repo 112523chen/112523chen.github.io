fb19a318b05fea988a0c027318ae615d84dca72d
Alex Chen <112623chen@gmail.com>
2023-01-16 01:15:27 -0500

    🚀 new(): add root

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import DisplayButton from "./components/DisplayButton/DisplayButton";
+import Main from "./components/Main/Main";
+import "./styles/main.css";
+import { projectData } from "./assets/data/projectData.json";
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer/Footer";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const colorState = window.matchMedia("(prefers-color-scheme:dark)").matches
+    ? "dark"
+    : "light";
+
+  const [mode, setMode] = useState<string>(colorState);
+
+  useEffect(() => {
+    if (mode === "dark") {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [mode]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DisplayButton mode={mode} setMode={setMode} />
+      <Main projectData={projectData} mode={mode} />
+      <Footer mode={mode} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
