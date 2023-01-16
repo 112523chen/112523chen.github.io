73dad1ddcc005b0b6f754aa8e345a5685002df13
Alex Chen <112623chen@gmail.com>
2023-01-16 01:00:36 -0500

    🚀 new(): Start Version 3

--- resources/js/courseworkChange.js
+++
@@ -1,5 +0,0 @@
-const course_div = document.getElementById("coursework");
-
-if (isMobileDevice) {
-  course_div.hidden = true;
-}
--- resources/js/titleChange.js
+++
@@ -1,7 +0,0 @@
-const docTitle = document.title;
-window.addEventListener("blur", () => {
-  document.title = "Alex Chen's Website";
-});
-window.addEventListener("focus", () => {
-  document.title = docTitle;
-});
