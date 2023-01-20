276f83cb5929e861192f9300b2124fb43c72b155
Alex Chen <112623chen@gmail.com>
2023-01-20 15:16:34 -0500

    💄 update(Display): fix UI

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,14 +4,8 @@ export const GlobalStyles = createGlobalStyle`
 html {
   scroll-behavior: smooth;
 }
-
 body {
   margin: 0;
   font-family: "Inter";
-  background-color: #fff;
-}
-body.dark-mode {
-  background-color: #111827;
-  color: #e7e7e7;
 }
 `;
