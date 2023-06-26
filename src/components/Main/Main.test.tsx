24317e45cfa2984a7c5c18137c42f5960d215113
Alex Chen <112623chen@gmail.com>
2023-06-26 12:29:26 -0400

    test

--- src/components/Main/Main.test.tsx
+++ src/components/Main/Main.test.tsx
@@ -7,20 +7,6 @@ import { projectData } from "../../assets/data/projectData.json";
 import "@testing-library/jest-dom";
 
 describe("Main Component", () => {
-  test("Coursework component is rendered when device is not mobile", () => {
-    let mode: LightState = "dark";
-    let theme: DefaultTheme = normalTheme;
-    let isMobile: boolean = false;
-
-    const { container } = render(
-      <ThemeProvider theme={theme}>
-        <Main projectData={projectData} isMobileDevice={isMobile} mode={mode} />
-      </ThemeProvider>
-    );
-
-    expect(container.childElementCount).toEqual(8);
-  });
-
   test("Coursework component is not rendered when device is mobile", () => {
     let mode: LightState = "dark";
     let theme: DefaultTheme = normalTheme;
