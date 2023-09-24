8cfe8a70d087d7db8e20e03a72d64e61dd76f539
Alex Chen <112623chen@gmail.com>
2023-09-24 16:51:17 -0400

    update(): add linkedin link

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { ContactBase, ContactParaph, EmailLink } from "./Contact.style";
+import {
+  ContactBase,
+  ContactParaph,
+  EmailLink,
+  LinkedInLink,
+} from "./Contact.style";
 import Heading from "../Heading/Heading";
 import { LightState } from "../model";
-import { Link } from "../Footer/Footer.style";
 
 interface Props {
   mode: LightState;
@@ -19,7 +23,15 @@ const Contact: React.FC<Props> = ({ mode }) => {
           <EmailLink mode={mode} href="mailto:itsalexchen@gmail.com">
             email
           </EmailLink>{" "}
-          but you can also connect with me on LinkedIn.
+          but you can also connect with me on{" "}
+          <LinkedInLink
+            mode={mode}
+            target="_blank"
+            href="https://www.linkedin.com/in/alex-chen-2407251b4/"
+          >
+            {" "}
+            LinkedIn
+          </LinkedInLink>
         </ContactParaph>
       </div>
     </ContactBase>
